0b241616b5a5090c2df0a7cc8d9ad1ad0ea1f883
RebeccaHA <rebeccahunt034@gmail.com>
2021-01-25 21:42:47 +0000

    set up app.vue with some dummy list data

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+    <li></li>
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      storedResoures: [
+        {
+          id: 'Official guide',
+          title: 'Official guide',
+          description: 'The Official vue documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'Google',
+          title: 'Google',
+          description: 'The Official google',
+          link: 'http://www.google.com'
+        }
+      ]
+    };
+  }
+};
+</script>
