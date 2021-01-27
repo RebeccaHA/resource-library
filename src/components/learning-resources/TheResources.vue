e01689778b8d5cb5aeede249a3fc98af02aca4ba
RebeccaHA <rebeccahunt034@gmail.com>
2021-01-27 17:10:40 +0000

    Added resource select tab to alternate between stored and add form

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <card>
+      <ui-button @click="selectTab('stored-resources')">Stored Resources</ui-button>
+      <ui-button @click="selectTab('add-resource')">Add Resources</ui-button>
+    </card>
+    <component :is="isSelected"></component>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isSelected: 'stored-resources'
+    };
+  },
+  methods: {
+    selectTab(tab) {
+      this.isSelected = tab;
+    }
+  }
+};
+</script>
