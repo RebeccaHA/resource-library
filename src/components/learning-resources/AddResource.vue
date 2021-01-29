6643f7a4c78f50f1062e5c9a8fd3cbe07b072738
RebeccaHA <rebeccahunt034@gmail.com>
2021-01-29 13:36:02 +0000

    set up error message component, when submitting form check if values are entered if not it shows error block

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -14,29 +14,46 @@
         <br />
         <ui-button type="submit">Submit</ui-button>
       </form>
+      <error-message v-if="invalidInput">
+        <h3>Please fill out all the fields</h3>
+        <ui-button>Close</ui-button>
+      </error-message>
     </card>
   </div>
 </template>
 
 <script>
+import ErrorMessage from '@components/UI/ErrorMessage';
 export default {
+  components: {
+    ErrorMessage
+  },
   inject: ['addResource'],
   data() {
     return {
       title: '',
       description: '',
-      link: ''
+      link: '',
+      invalidInput: false
     };
   },
   methods: {
     submitForm() {
-      const resource = {
-        title: this.title,
-        description: this.description,
-        link: this.link
-      };
-      this.addResource(resource);
-      console.log(resource);
+      if (this.title === '') {
+        this.invalidInput === true;
+      } else if (this.description === '') {
+        this.invalidInput === true;
+      } else if (this.link === '') {
+        this.invalidInput === true;
+      } else {
+        const resource = {
+          title: this.title,
+          description: this.description,
+          link: this.link
+        };
+        this.addResource(resource);
+        console.log(resource);
+      }
     }
   }
 };
